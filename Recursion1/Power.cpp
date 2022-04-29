db50103a22d7fb409998e75d4ba9fa850c064feb
Ankit Kumar <88185950+ankitkr47@users.noreply.github.com>
2022-04-30 00:31:44 +0530

    Update Power.cpp

--- Recursion1/Power.cpp
+++ Recursion1/Power.cpp
@@ -1,4 +1,4 @@
-`int power(int x, int n) {
+int power(int x, int n) {
   /* Don't write main().
      Don't read input, it is passed as function argument.
      Return output and don't print it.
@@ -8,4 +8,3 @@
     return x*power(x,n-1);
 
 }
-`
